8546b8e1b9caf104b014e924b9a1ed236132c408
paramdevelopmentsg-dev <paramdevelopmentsg@gmail.com>
2025-11-17 12:08:42 +0800

    Update demo-router-function_tools.py

--- demo-router-function_tools.py
+++ demo-router-function_tools.py
@@ -1,4 +1,27 @@
+from trace import Trace
+from dotenv import load_dotenv
+from agents import Agent, Runner, trace
+from openai import AsyncOpenAI
+import os
+from pydantic import BaseModel
+from datetime import datetime
 
+
+load_dotenv(override=True)
+
+
+google_api_key = os.getenv('GOOGLE_API_KEY')
+
+
+if google_api_key:
+    print(f"google API Key exists and begins {google_api_key[:6]}")
+else:
+    print("Google API Key not set - please head to the troubleshooting guide in the setup folder")
+
+
+GEMINI_BASE_URL = "https://generativelanguage.googleapis.com/v1beta/openai/"
+gemini_client = AsyncOpenAI(base_url=GEMINI_BASE_URL, api_key=google_api_key)
+gemini_model = OpenAIChatCompletionsModel(model="gemini-2.0-flash", openai_client=gemini_client)
 
 
 
